1b735af51c177d56c07fe251c7468be1b0bbbd43
dePaula06 <p.depaula06.sp@gmail.com>
2025-05-14 22:10:19 -0300

    Correção exercício 2

--- FirstSemester/ProgramacaoComputadores/repeticao/companhiaTeatro/js/index.js
+++ FirstSemester/ProgramacaoComputadores/repeticao/companhiaTeatro/js/index.js
@@ -2,22 +2,41 @@ function companhiaTeatro() {
 
     let precoIngresso
     let ingressosVendidos
-    let despesas
     let lucroMaximo
+    const despesas = 200 
+    let aux
+    let maiorLucro
+    let melhorQuantidade
+    let melhorPreco
+
+    maiorLucro = 0
+    melhorQuantidade = 0
+    melhorPreco = 0
+
+    aux = ""
 
     precoIngresso = 5
     ingressosVendidos = 120
-    despesas = 200
 
     alert(`Olá, vamos juntos analisar as vendas e os lucros de acordo com o preço dos ingressos!`)
 
     while (precoIngresso >= 1) {
         lucroMaximo = (ingressosVendidos * precoIngresso) - despesas
-        alert(`Com o preço de R$${precoIngresso}\n` +
-              `E o máximo de ingressos vendidos igual a ${ingressosVendidos}\n` +
-              `Podemos ter um lucro máximo de R$${lucroMaximo}`
+        aux = aux + "\n\n" + (`Preço: R$${precoIngresso} - ` +
+              `quantidade: ${ingressosVendidos} - ` +
+              `lucro máximo: R$${lucroMaximo}`
         );
+        if(lucroMaximo > maiorLucro){
+            maiorLucro = lucroMaximo;
+            melhorQuantidade = ingressosVendidos;
+            melhorPreco = precoIngresso
+        }
         precoIngresso = precoIngresso - 0.5
         ingressosVendidos += 26
     }
+
+    alert(aux)
+    alert(`O melhor preço é: R$${melhorPreco}\n` +
+    `Com quantidade vendida igual a: ${melhorQuantidade}\n` +
+    `E lucro máximo de: R$${maiorLucro}`)
 }
