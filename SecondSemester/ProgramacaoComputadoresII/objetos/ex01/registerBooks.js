bd6085a57d949786947e000f1131eb8be6757bc7
dePaula06 <p.depaula06.sp@gmail.com>
2025-08-06 20:34:30 -0300

    escopo primeiro exercício

---
+++ SecondSemester/ProgramacaoComputadoresII/objetos/ex01/registerBooks.js
@@ -0,0 +1,30 @@
+/*
+
+Exercício – Cadastro de Livros e Filtro por Páginas
+
+Crie um programa em JavaScript que:
+
+    1. Leia os dados de 5 livros digitados pelo usuário. Para cada livro, devem ser informados:
+    título (string)
+    autor (string)
+    ano de publicação (número)
+    quantidade de páginas (número)
+
+    2. Armazene os livros em um vetor de objetos.
+
+    3. Ao final, o programa deve:
+    Exibir todos os livros com mais de 300 páginas.
+    Exibir o título do livro com maior número de páginas.
+    Calcular e exibir a média de páginas dos livros cadastrados.
+
+*/
+function registerBooks() {
+
+    let library = []
+    let i
+    let book
+    let mais300
+    let maior
+    let mediaPages
+
+}
