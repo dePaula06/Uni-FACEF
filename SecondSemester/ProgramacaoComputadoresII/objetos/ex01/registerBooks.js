9b054957df122d2c294ce8d7ed400c09a9532c86
dePaula06 <p.depaula06.sp@gmail.com>
2025-08-09 14:27:02 -0300

    Exercício 01 finalizado

--- SecondSemester/ProgramacaoComputadoresII/objetos/ex01/registerBooks.js
+++ SecondSemester/ProgramacaoComputadoresII/objetos/ex01/registerBooks.js
@@ -22,9 +22,66 @@ function registerBooks() {
 
     let library = []
     let i
-    let book
-    let mais300
-    let maior
-    let mediaPages
+    let book, title, autor, year, pages
+    let mais300 = []
+    let maior, maiorTitle
+    let mediaPages = 0
+    
+    for( i = 0; i < 5; i++){
+
+        // lendo os dados
+
+        title = prompt(`Qual o título do ${i+1}º livro: `)
+        autor = prompt(`Qual o autor do livro ${title}: `)
+        year = Number(prompt(`Qual o ano de lançamento do livro ${title}: `))
+        pages = Number(prompt(`Quantas páginas ele tem: `))
+
+        book = {
+            title: title,
+            autor: autor,
+            year: year,
+            pages: pages
+        };
+
+        // armazenando em um vetor
+
+        library.push(book);
+    }
+
+    // Armazenamento e exibição de livros com mais de 300 páginas
+
+    for( i = 0; i < 5; i++){
+        if(library[i].pages > 300){
+            mais300.push(library[i])
+        }
+    }
+    console.log(`Livros com mais de 300 páginas: ${mais300}`);
+
+    // Processamento e exibição do título do livro com mais páginas
+
+    maior = library[0].pages
+    maiorTitle = library[0].title
+
+    for( i = 1; i < 5; i++){
+        if( maior < library[i].pages){
+            maiorTitle = library[i].title
+            maior = library[i].pages
+        }
+    }
+
+    console.log(`O livro com o maior número de páginas é: ${maiorTitle}, com ${maior} páginas`);
+
+    // Somando número de páginas, fazendo a média e exibindo
+
+    for( i = 0; i < 5; i++){
+        mediaPages += library[i].pages
+    }
+
+    mediaPages = mediaPages / 5
+
+    console.log(`A média de páginas é: ${mediaPages}`);
+    
+    
+    
 
 }
