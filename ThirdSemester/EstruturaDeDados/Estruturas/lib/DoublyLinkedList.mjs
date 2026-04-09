9dc7862892183182c2919aefd78511f5d9fc25b2
PEDRO DE PAULA OLIVEIRA <26367@unifacef.edu.br>
2026-04-08 22:13:17 -0300

    Finalizando conceitos de Lista duplamente encadeada, com testes

--- ThirdSemester/EstruturaDeDados/Estruturas/lib/DoublyLinkedList.mjs
+++ ThirdSemester/EstruturaDeDados/Estruturas/lib/DoublyLinkedList.mjs
@@ -1,243 +1,212 @@
 class Node {
-
-    constructor(val) {
-        this.prev = null;
-        this.data = val;
-        this.next = null
-    }
-
+  constructor(val) {
+    this.prev = null;
+    this.data = val;
+    this.next = null;
+  }
 }
 
 export default class DoublyLinkedList {
+  #head;
+  #tail;
+  #count;
 
-    #head
-    #tail
-    #count
+  constructor() {
+    this.#head = null;
+    this.#tail = null;
+    this.#count = 0;
+  }
 
-    constructor() {
-        this.#head = null;
+  get isEmpty() {
+    return this.#count === 0;
+  }
+
+  get count() {
+    return this.#count;
+  }
+
+  //método PRIVADO que encontra um nodo por sua posição
+  #findNode(pos) {
+    let node;
+    //nodo encontra-se na primeira metade da lista
+    if (pos < this.#count / 2) {
+      node = this.#head;
+      for (let i = 0; i < pos; i++) {
+        node = node.next;
+      }
+      //nodo encontra-se na segunda metade da lista
+    } else {
+      node = this.#tail;
+      for (let i = this.#count - 1; i > pos; i--) {
+        node = node.prev;
+      }
+    }
+    return node;
+  }
+
+  //método para inserir em qualquer posição
+  insert(pos, val) {
+    let inserted = new Node(val);
+
+    //1º caso: lista vazia
+    if (this.isEmpty) {
+      this.#head = inserted;
+      this.#tail = inserted;
+    }
+
+    //2º caso: inserção na primeira posição
+    else if (pos === 0) {
+      inserted.next = this.#head;
+      this.#head.prev = inserted;
+      this.#head = inserted;
+    }
+
+    //3º caso: inserção na última posição
+    else if (pos >= this.#count) {
+      inserted.prev = this.#tail;
+      this.#tail.next = inserted;
+      this.#tail = inserted;
+    }
+
+    //4º caso: inserção em posição intermediaria
+    else {
+      let nodePos = this.#findNode(pos);
+      let before = nodePos.prev;
+
+      before.next = inserted;
+
+      inserted.prev = before;
+
+      inserted.next = nodePos;
+
+      nodePos.prev = inserted;
+    }
+
+    this.#count++;
+  }
+
+  //método de atalho para inserção na primeira posição
+  insertHead(val) {
+    this.insert(0, val);
+  }
+
+  //método de atalho para inserção na últuma posição
+  insertTail(val) {
+    this.insert(this.#count, val);
+  }
+
+  //método para remover um nodo da lista
+  remove(pos) {
+    //1º caso: lista vazia ou  posição fora dos limites
+    if (this.isEmpty || pos < 0 || pos > this.#count - 1) {
+      return undefined;
+    }
+
+    let removed;
+    //2º caso: remoção do primeiro nodo
+    if (pos === 0) {
+      removed = this.#head;
+      this.#head = removed.next;
+
+      if (this.#head) {
+        this.#head.prev = null;
+      }
+
+      if (this.#count === 1) {
         this.#tail = null;
-        this.#count = 0;
+      }
     }
 
-    get isEmpty() {
-        this.#count === 0;
-    }
-    
+    //3º caso: remoção do último nodo (tail)
+    else if (pos === this.#count - 1) {
+      removed = this.#tail;
 
-    get count() {
-        return this.#count;
-    }
+      this.#tail = removed.prev;
 
+      if (this.#tail) {
+        this.#tail.next = null;
+      }
 
-    // Método privado - encontra um nodo por sua posição
-    #findNode(pos) {
-        let node;
-        // nodo encontra-se na primeira metade da lista
-        if (pos < this.#count / 2) {
-            node = this.#head
-            for (let i = 0; i < pos; i++) {
-                node = node.next;
-            }
-        }// nodo encontra-se na segunda metade da lista 
-        else {
-            node = this.#tail
-            for (let i = (this.#count - 1); i > pos; i--) {
-                node = node.prev
-            }
-        }
-
-        return node
+      if (this.#count === 1) {
+        this.#head = null;
+      }
     }
 
-    // Método para inserir em qualquer posição
-    insert(pos, val) {
+    //4º caso: remoção em posição intermediária
+    else {
+      removed = this.#findNode(pos);
+      let before = removed.prev;
+      let after = removed.next;
 
-        let inserted = new Node(val);
-
-        // 1º caso: lista vazia
-        if (this.isEmpty) {
-
-            this.#head = inserted;
-            this.#tail = inserted;
-
-        }
-
-        //2º caso: inserção na primeira posição
-        else if (pos === 0) {
-
-            inserted.next = this.#head;
-            this.#head.prev = inserted;
-            this.#head = inserted;
-
-        }
-
-        // 3º caso: inserção na última posição
-        else if (pos >= this.#count) {
-
-            inserted.prev = this.#tail;
-            this.#tail.next = inserted;
-            this.#tail = inserted;
-
-        }
-
-        // 4º caso: inserção em posição intermediária
-        else {
-
-            let nodePos = this.#findNode(pos);
-            let before = nodePos.prev
-
-            before.next = inserted
-            inserted.prev = before
-            inserted.next = nodePos
-            nodePos.prev = inserted
-
-        }
-
-        this.#count++
-
+      before.next = after;
+      after.prev = before;
     }
 
-    // metodo de atalho para inserção na primeira posição
-    insertHead(val) {
-        this.insert(0, val)
+    this.#count--;
+
+    return removed.data;
+  }
+
+  //método de atalho para remoção da primeira posição
+  removeHead() {
+    return this.remove(0);
+  }
+
+  //método de atalho para remoção da última posição
+  removeTail() {
+    return this.remove(this.#count - 1);
+  }
+
+  peek(pos) {
+    //lista vazia ou posição fora dos limites
+    if (this.isEmpty || pos < 0 || pos > this.#count - 1) {
+      return undefined;
+    }
+    const node = this.#findNode(pos)
+    return node.data
+  }
+
+  peekHead(){
+    return this.peek(0)
+  }
+
+  peekTail(){
+    return this.peek(this.#count - 1)
+  }
+
+  //método que retorna a posição do nodo cujo conteúdo foi especificado
+  indexOf(val){
+    const middle = Math.ceil(this.#count / 2)
+    let node1 = this.#head //começa na cabeça da lista
+    let node2 = this.#tail //começa na cauda da lista
+
+    for(let pos = 0; pos < middle; pos++){
+      //verifica se o valor está no node1
+      if(val === node1.data) return pos
+      //verifica se o valor está no node2
+      if(val === node2.data) return this.#count - 1 - pos
+
+      //node1 avança via next
+      node1 = node1.next
+
+      //node2 retocede via prev
+      node2 = node2.prev
     }
 
-    // metodo de atalho para inserção na última posição
-    insertTail(val) {
-        this.insert(this.#count, val)
+    return -1
+  }
+
+
+  print(){
+    let output = '( '
+    let node = this.#head
+
+    for(let i = 0; i < this.#count; i++){
+      if(output !== '( ') output += ', '
+      output += `[${i}]: ${node.data}`
+      node = node.next
     }
-
-    // metodo para remover um nodo da lista
-    remove(pos) {
-        // 1º caso: lista vazia ou posição fora dos limites
-        if (this.isEmpty || pos < 0 || pos > this.#count - 1) {
-            return undefined
-        }
-
-        // 2º caso: remoção do primeiro nodo
-        let removed
-
-        if (pos === 0) {
-
-            removed = this.#head
-            this.#head = removed.next
-
-
-            if (this.#head) {
-                this.#head.prev = null
-            }
-
-            if (this.#count === 1) {
-                this.#tail = null
-            }
-        }
-
-        // 3º caso: remoção do último nodo
-        else if( pos === this.#count - 1){
-
-            removed = this.#tail
-            this.#tail = removed.prev
-
-            if(this.#tail){
-                this.#tail.next = null
-            }
-
-            if(this.#count === 1){
-                this.#head = null
-            }
-
-        }
-
-        // 4º caso: remoção em posição intermediária
-        else {
-
-            removed = this.#findNode(pos)
-            let before = removed.prev
-            let after = removed.next
-
-            before.next = after
-            after.prev = before
-
-        }
-
-        this.#count--
-
-        return removed.data
-    }
-
-    // metodo de atalho da primeira posição
-    removeHead(){
-
-        return this.remove(0)
-
-    }
-
-    // metodo de atalho da última posição
-    removeTail(){
-
-        return this.remove(this.#count - 1)
-        
-    }
-
-    peek(pos){
-
-        // lista vazia ou posição fora dos limites
-        if (this.isEmpty || pos < 0 || pos > this.#count - 1) {
-            return undefined
-        }
-
-        const node = this.#findNode(pos);
-        return node.data
-
-    }
-
-    peekHead(){
-        return this.peek(0)
-    }
-
-    peekTail(){
-        return this.peek(this.#count - 1)
-    }
-
-    // metodo que retorna a posição do nodo cujo conteúdo foi especificado
-    indexOf(val) {
-
-        const middle = Math.ceil(this.#count / 2)
-        let node1 = this.#head
-        let node2 = this.#tail
-
-        for(let pos = 0; pos < middle; pos++){
-            // verifica se o valor está no node1
-            if(val === node1.data){
-                return pos
-            }
-            // verifica se o valor está no node2
-            if(val === node2.data){
-                return this.#count - 1 - pos
-            }
-
-            node1 = node1.next
-            node2 = node2.prev
-
-        }
-        
-        return - 1
-
-    }
-
-    print(){
-        let output = '('
-        let node = this.#head
-        for(let i = 0; i < this.#count; i++) {
-
-            if(output !== '( ') output += ', '
-            output += `[${i}: ${node.data}]`
-
-            node = node.next
-
-        } output += ` ), count: ${this.#count}`
-        return output 
-    }
-
+    output += ` ), count: ${this.#count}`
+    return output
+  }
 }
